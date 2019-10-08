ae4f5908d7511629f5dad20d917ed31d61d08f23
PandaGears <seraphmite1@gmail.com>
2019-10-08 21:49:00 +0200

    day00

---
+++ Day00/Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	static public int Speed = 0;
+	static public bool direct = true;
+	static public bool end = false;
+
+	void Start () {
+	
+	}
+	void Update () {
+		if (Club.Force > 0 && Input.GetKeyUp (KeyCode.Space)) {
+	
+			Speed = Club.Force;
+			direct = true;
+			Club.Force = 0;
+		}
+		if (Speed > 0) {
+			if (transform.position.y >= 20) {
+				direct = false;
+			}
+			else if (transform.position.y <= -13.5F) {
+				direct = true;
+			}
+			if (direct == true) {
+				transform.Translate(0, 0.4F, 0);
+				Speed -= 1;
+			}
+			else {
+				transform.Translate(0, -0.4F, 0);
+				Speed -= 1;
+			}
+		}
+		if (Speed < 5 && Speed >= 0) {
+			if (transform.position.y > 17 && transform.position.y < 18){
+				GameObject.Destroy(gameObject);
+				end = true;
+				if (Club.Score <= 0)
+					Debug.Log ("Score:" + Club.Score);
+				else
+					Debug.Log ("Score:" + Club.Score);
+
+			}
+		}
+	}
+}
---
+++ Day00/Assets/Scripts/Club.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Club : MonoBehaviour {
+	
+	static public int Score = -15;
+	static public int Force = 0;
+	static public bool keyPressed = false;
+	static public bool BallBouncing = false;
+	static public float Defautclub = -0.5F;
+	public GameObject BallsImage;
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		if (Ball.end == true)
+			GameObject.Destroy(gameObject);
+
+		if (BallBouncing == true && Ball.Speed == 0 && BallsImage != null) {
+			if (transform.position.y != (BallsImage.transform.position.y - 0.2)) {
+				transform.position = new Vector3(-1, (BallsImage.transform.position.y - 0.2F), 0);
+				Defautclub = BallsImage.transform.position.y - 0.2F;
+				BallBouncing = false;
+			}
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			keyPressed = true;
+			if (transform.position.y > -11)
+				transform.Translate(0F, -0.02F, 0F);
+			
+		}
+
+		else if (Input.GetKeyUp (KeyCode.Space)) {
+			keyPressed = false;
+			if (transform.position.y < Defautclub) {
+				while (transform.position.y < Defautclub)
+					transform.Translate(0F, +0.02F, 0F);
+				Score += 5;
+				Debug.Log ("score: " + Score);
+				BallBouncing = true;
+			}
+		}
+
+		if (keyPressed == true) {
+
+			if (transform.position.y > -2.15)
+			{
+				transform.Translate(0F, -0.02F, 0F);
+				Force += 1;
+			}
+		}
+	}
+}
