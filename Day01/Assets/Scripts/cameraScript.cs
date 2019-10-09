9d2c5e967c617af9491c250e7895d940be79fa1f
PandaGears <seraphmite1@gmail.com>
2019-10-09 18:38:30 +0200

    commitment issue

---
+++ Day01/Assets/Scripts/cameraScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cameraScript : MonoBehaviour {
+	public static playerScript_ex00 controller;
+	private GameObject characters;
+
+	void Start () {
+
+		characters = GameObject.FindGameObjectWithTag("Thomas");
+
+	}
+
+	void Update () {
+		if (playerScript_ex00.GetController == 0) {
+
+			if (tag == "camera1")
+				characters = GameObject.FindGameObjectWithTag ("Claire");
+			else if (tag == "camera2")
+				characters = GameObject.FindGameObjectWithTag ("John");
+			else
+				characters = GameObject.FindGameObjectWithTag ("Thomas");
+		}
+		else if (playerScript_ex00.GetController == 1) {
+
+			if (tag == "camera1")
+				characters = GameObject.FindGameObjectWithTag ("Claire");
+			else if (tag == "camera2")
+				characters = GameObject.FindGameObjectWithTag ("Thomas");
+			else
+				characters = GameObject.FindGameObjectWithTag ("John");
+		}
+		else {
+
+			if (tag == "camera1")
+				characters = GameObject.FindGameObjectWithTag ("Thomas");
+			else if (tag == "camera2")
+				characters = GameObject.FindGameObjectWithTag ("John");
+			else
+				characters = GameObject.FindGameObjectWithTag ("Claire");
+		}
+		if (characters != null) {
+			transform.position = characters.transform.position;
+			transform.Translate (0, 0, -10);
+		}
+	}
+}
---
+++ Day01/Assets/Scripts/moveHorizontal.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveHorizontal : MonoBehaviour {
+	
+
+	private bool top = false;
+
+	private float offset = -3.5F;
+	private bool go = false;
+	private int count = -100;
+	private int endtime;
+	private bool first = true;
+	void Start () {
+	}
+
+	void Update () {
+		if (first) {
+			endtime = Random.Range (-100, 0);
+			first = false;
+		}
+		if (count == endtime)
+			go = true;
+		count += 1;
+		if (go == true) {
+			if (offset < 0 && !top) {
+				transform.position += Vector3.right * Time.deltaTime;
+				offset += 0.1F;
+				
+			} else {
+				top = true;
+				transform.position += Vector3.left * Time.deltaTime;
+				offset -= 0.1F;
+			}
+			if (offset < -3.5)
+				top = false;
+		}
+	} 
+}
