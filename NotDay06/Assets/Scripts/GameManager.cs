f0f328c5dae1f97a34ba617ed3a891fa695ce378
PandaGears <37896398+PandaGears@users.noreply.github.com>
2019-10-16 14:43:39 +0200

    Add files via upload

---
+++ NotDay06/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public player player;
+	public static GameManager gm;
+	public Text message;
+
+	void Start () {
+		if (gm == null)
+			gm = this;
+		message.GetComponent<Animator> ().SetTrigger ("showText");
+	}
+
+	void Update () {
+		
+	}
+
+	public void cameraSpotPlayer() {
+		player.playerSpotted (20);
+	}
+
+	public void setMessage(string msg) {
+		message.text = msg;
+		message.GetComponent<Animator> ().SetTrigger ("showText");
+	}
+}
---
+++ NotDay06/Assets/Scripts/keyCard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keyCard : MonoBehaviour {
+
+
+	void Start () {
+		StartCoroutine ("rotate");
+	}
+
+	void Update () {
+		
+	}
+
+	IEnumerator rotate() {
+		while (true) {
+			transform.Rotate (Vector3.up);
+			yield return new WaitForSeconds(0.01f);
+		}
+	}
+}
